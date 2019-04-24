468f146ee926cffa0cff5a07593fdc9b27f50c61
Tatsuya Yoshida <alpacagame0129@gmail.com>
2019-04-25 08:14:57 +0900

    Add: 画像を時間で取得

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,7 @@
 class HomesController < ApplicationController
+  def index
+    t = Time.new
+    @now = t.strftime("%Y-%m-%d %H:%M")
+    @url = "http://lorempixel.com/400/200/cats/#{t.min % 10}"
+  end
 end
