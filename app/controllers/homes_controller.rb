3c99dab26b7b46d1c864d19f093217a0d72f3cc1
Tatsuya Yoshida <alpacagame0129@gmail.com>
2019-04-25 13:55:22 +0900

    Add: style

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,7 @@
 class HomesController < ApplicationController
   def index
     t = Time.new
-    @now = t.strftime("%Y-%m-%d %H:%M")
+    @now = t.strftime("%Y年%m月%d日 %H時%M分")
     @url = "http://lorempixel.com/400/200/cats/#{t.min % 10}"
   end
 end
